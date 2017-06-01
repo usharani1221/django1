b63b32353fd8eb05b08e453a57f8b1b8f5d96bf8
usharani1221 <usharani@gvpce.ac.in>
2017-06-01 09:51:40 +0530

    first commit

---
+++ rango/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return HttpResponse('<a href="/rango/">Index</a>')
